55882c6eb8de25df11deaeff970724200c13386d
Krisjanis Balodis <KrisjanisB1@verifone.com>
2018-11-20 15:59:37 +0200

    fix

--- MD_32.pl
+++ MD_32.pl
@@ -1,6 +1,6 @@
 aa(A,B,C) :-
     rmdups(A,B,X),
-    list_set(X,C).
+    list(X,C).
 
 rmdups([],_D,[]).
 rmdups([H|T],D,Vs) :-    
@@ -15,13 +15,14 @@ rmdup(K,[(X,_)|P],Vs) :-
     dif(K,X),
     rmdup(K,P,Vs).
 
-list_set([],[]).
-list_set([X|Xs],[X|Ys]) :-
+list([],[]).
+list([X|Xs],[X|Ys]) :-
     subtract(Xs,[X],Zs),
-    list_set(Zs,Ys).
+    list(Zs,Ys).
 
 cc(A,B,C) :-
-    matching(A,B,C).
+    matching(A,B,X),
+    list(X,C).
 
 matching([],_,[]).
 matching([Pair|P1], T, Z) :-
@@ -33,4 +34,7 @@ match(_,[],[]).
 match((K1,V1),[(K2,V2)|P2],[Pair|Z]) :-
     V1 == K2,
     Pair = (K1,V2),
+    match((K1,V1), P2, Z).
+match((K1,V1),[(K2,_)|P2],Z) :-
+    V1 \= K2,
     match((K1,V1), P2, Z).
